f498847f9212ddab7b81e4b6e9cabab4c59df764
osama <osama.mahmoud1697@gmail.com>
2026-03-31 11:38:52 +0200

    add Todo application

--- 01-Selenium_Java_TarekRoshdy/POM/src/test/java/LoginPageTest.java
+++
@@ -1,8 +0,0 @@
-import org.openqa.selenium.WebDriver;
-
-public class LoginPageTest {
-
-    private WebDriver driver;
-
-    LoginPage loginPage = new LoginPage(driver);
-}
--- 02-Selenium WebDriver From Foundation to Framework [In Arabic]/Java/src/main/java/courses/Animal.java
+++
@@ -1,7 +0,0 @@
-package courses;
-
-abstract class Animal {
-
-    // abstract method (does not have a body)
-    public abstract void animalSound();
-}
